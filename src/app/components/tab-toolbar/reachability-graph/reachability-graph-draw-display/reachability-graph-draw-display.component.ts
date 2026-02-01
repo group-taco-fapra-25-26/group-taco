44aa90f038a7c8be9b0f9e096d09480c41346891
Alex <alexkolbfleisch@gmx.de>
2026-02-01 11:47:45 +0100

    [FPWGT-102] adjust viewmode to be descriptive in learning Mode and simple in exam mode.

--- src/app/components/tab-toolbar/reachability-graph/reachability-graph-draw-display/reachability-graph-draw-display.component.ts
+++ src/app/components/tab-toolbar/reachability-graph/reachability-graph-draw-display/reachability-graph-draw-display.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
 import { SvgStateNodeComponent } from '../../../display/svg-state-node/svg-state-node.component';
 import { SvgStateArcComponent } from '../../../display/svg-state-arc/svg-state-arc.component';
 import { PanningService } from 'src/app/services/panning.service';
@@ -11,6 +11,8 @@ import {
 } from '../../../draw-toolbar/draw-toolbar.component';
 import { DisplayableNode } from '../../../../classes/displayable-graph.interface';
 import { StateNode } from '../../../../classes/reachability-graph.model';
+import { ModeService } from '../../../../services/mode.service';
+import { Tab } from '../../../../classes/tabs';
 
 @Component({
     selector: 'app-reachability-graph-draw-display',
@@ -24,7 +26,18 @@ export class ReachabilityGraphDrawDisplayComponent extends DisplayComponent {
     protected override graphId = GRAPH_IDS.REACHABILITY;
     readonly reachabilityGraphDiagram = this._reachabilityGraphService.reachabilityGraphSignal;
     readonly isEmpty = computed(() => this.reachabilityGraphDiagram().nodes.length === 0);
-    readonly viewMode = signal<ViewMode>(VIEW_MODES.SIMPLE);
+    readonly viewMode = signal<ViewMode>(VIEW_MODES.DESCRIPTIVE);
+    readonly modeService = inject(ModeService);
+
+    constructor() {
+        super();
+        effect(() => {
+            const isExamSignal = this.modeService.getIsExamModeSignal(Tab.REACHABILITY_GRAPH);
+            if (isExamSignal && isExamSignal()) {
+                this.viewMode.set(VIEW_MODES.SIMPLE);
+            }
+        });
+    }
 
     private draggedNode: DisplayableNode | null = null;
     private dragOffset = { x: 0, y: 0 };
