9d54b2964942aca4d81758d2d5a569ff901ba171
Alexander Kolbfleisch <alexkolbfleisch@gmx.de>
2026-02-05 14:42:03 +0100

    fix small performance problem + adjust a translation + fix tuple behavior + fix icons to be consistent

--- src/app/components/tuple-input/tuple-input.component.ts
+++ src/app/components/tuple-input/tuple-input.component.ts
@@ -13,6 +13,11 @@ import { SpringEmbedderService } from '../../services/spring-embedder.service';
 import { DisplayService } from '../../services/display.service';
 import { DiagramNode } from '../../classes/diagram/diagram-node';
 import { applyParallelOffsetsToArcs } from '../../services/arc-parallel-offset.util';
+import { TabStateService } from '../../services/tab-state.service';
+import { ProcessNetStateService } from '../../services/process-net-state.service';
+import { ReachabilityGraphService } from '../../reachability-graph.service';
+import { Tab } from '../../classes/tabs';
+import { PanningService } from '../../services/panning.service';
 
 @Component({
     selector: 'app-tuple-input',
@@ -38,6 +43,10 @@ export class TupleInputComponent {
     private _displayService = inject(DisplayService);
     private _toaster = inject(ToasterNotificationService);
     private _dialogRef = inject(MatDialogRef<TupleInputComponent>);
+    private _tabStateService = inject(TabStateService);
+    private _processNetStateService = inject(ProcessNetStateService);
+    private _reachabilityGraphService = inject(ReachabilityGraphService);
+    private _panningService = inject(PanningService);
 
     processTuple() {
         if (!this.tupleString) return;
@@ -45,6 +54,15 @@ export class TupleInputComponent {
         const diagram = this._parserService.parse(this.tupleString);
         if (diagram) {
             this._sourcePetriNetService.loadNewNet(diagram, this.tupleString);
+
+            const currentTab = this._tabStateService.currentTab();
+            if (currentTab === Tab.PROCESS_NET) {
+                this._processNetStateService.clear();
+                this._processNetStateService.createStartPositions(diagram, this._panningService.INITIAL_VIEWBOX);
+            } else if (currentTab === Tab.REACHABILITY_GRAPH) {
+                this._reachabilityGraphService.clear();
+            }
+
             this._displayService.display(diagram);
             this._springEmbedderService.calculateLayout().catch((error) => console.error(error));
             this._toaster.showSuccess('TUPLE_INPUT.TOAST_SUCCESS_HEADER', 'TUPLE_INPUT.TOAST_SUCCESS_BODY');
