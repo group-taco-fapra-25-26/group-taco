3514a795bbe05dcf217bb2167b1e07653aeb3629
Alexander Kolbfleisch <alexkolbfleisch@gmx.de>
2026-02-06 10:58:02 +0100

    remove unnecessary console log calls

--- src/app/services/process-net-state.service.ts
+++ src/app/services/process-net-state.service.ts
@@ -70,10 +70,7 @@ export class ProcessNetStateService {
         this.connections.set([]);
         this.elementIdCounter = 0;
         this.connectionIdCounter = 0;
-        console.log('Diagram cleared. Mode:', mode);
-        console.log(diagram);
         if (mode === AppMode.LEARN && diagram) {
-            console.log('Creating start positions for diagram in LEARN mode');
             this.createStartPositions(diagram, viewBoxValues);
         }
     }
